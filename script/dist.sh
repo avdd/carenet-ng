4337091dbf1e60325ab5933ee40004761ce3a4d9
avdd <i@avdd.co>
2015-07-21 19:57:43 +1000

    use git revision in upload

--- script/dist.sh
+++ script/dist.sh
@@ -5,7 +5,13 @@ set -eu
 root=$(readlink -f $(dirname $0)/..)
 host=rev
 # TODO: distinct there location based on branch
-there=public_html/carenet-ng
+
+release=$(git describe --tags)
+branch=$(git rev-parse --abbrev-ref HEAD)-
+dirty=$(git diff --no-ext-diff --quiet --exit-code || echo '+')
+
+id="$branch$release$dirty"
+there=public_html/carenet-ng/$id
 dist=$root/.run/carenet-gulp-tmp/dist
 upgrade_html=$root/src/client/upgrading.html
 index_html=index.html
@@ -28,6 +34,7 @@ assets=$there/$(basename $dist/assets-*)
 
 RSYNC='rsync -v'
 
+ssh $host "mkdir -p $there"
 $RSYNC $upgrade_html $host:$there/$index_html
 $RSYNC -ra --delete $dist/assets-*/  $host:$there/'assets-*/'
 ssh $host "test -d $assets || mv -v $there/assets-* $assets"
